629753e9d25d823d72d9e0974e12563438a0b904
Gavin Labino <gavinlabino@gmail.com>
2020-10-17 20:09:22 +0000

    I finished Terrapin card

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,48 @@
 public class TerrapinCard {
+private double balance;
+
+public TerrapinCard(double balance){
+this.balance=balance;
+}
+
+public String toString(){
+  String money = "This card has " + balance + " dollars.";
+  return money;
+}
+
+public void payEconomical(){
+  if(balance < 2.50){
+    balance = balance;
+  }else{
+    balance-=2.50;
+  } 
+}
+
+public void payGourmet(){
+ if(balance < 4.00){
+    balance = balance;
+}else{
+    balance-=4.00;
+}
+}
+
+public void loadMoney(double amount){
+  if(amount > 150){
+    balance = 150;
+  }
+
+  if(amount < 0){
+    balance = balance;
+  }else if((amount + balance < 150)){
+    balance+=amount;
+  }
+
+}
+
+public double getBalance(){
+  return balance;
+}
+
 
 
 }
