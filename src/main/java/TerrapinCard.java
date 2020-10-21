11b3be10aaa77d078fc036fe535ec55ad2a85d87
Gavin Labino <gavinlabino@gmail.com>
2020-10-21 13:41:15 +0000

    messed around with loadmoney

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,47 +1,51 @@
 public class TerrapinCard {
-private double balance;
+  private double balance;
 
-public TerrapinCard(double balance){
-this.balance=balance;
-}
+  public TerrapinCard(double balance){
+    this.balance=balance;
+  }
 
-public String toString(){
-  String money = "The card has " + balance + " dollars";
-  return money;
-}
+  public String toString(){
+    String money = "The card has " + balance + " dollars";
+    return money;
+  }
 
-public void payEconomical(){
-  if(balance < 2.50){
-    balance = balance;
+  public void payEconomical(){
+    if(balance < 2.50){
+      balance = balance;
+    }else{
+      balance-=2.50;
+    } 
+  }
+
+  public void payGourmet(){
+  if(balance < 4.00){
+      balance = balance;
   }else{
-    balance-=2.50;
-  } 
-}
-
-public void payGourmet(){
- if(balance < 4.00){
-    balance = balance;
-}else{
-    balance-=4.00;
-}
-}
-
-public void loadMoney(double amount){
-  if(amount > 150.0){
-    balance = 150.0;
+      balance-=4.00;
+  }
   }
 
-  if(amount < 0){
-    balance = balance;
-  }else if((amount + balance < 150.0)){
-    balance+=amount;
+  public void loadMoney(double amount){
+    if(amount < 0){
+      balance = balance;
+    }else if((amount + balance > 150)){
+      balance = 150.0;
+    }else{
+      balance+=amount;
+    }
+
+
+
+
+
+
+
   }
 
-}
-
-public double getBalance(){
-  return balance;
-}
+  public double getBalance(){
+    return balance;
+  }
 
 
 
